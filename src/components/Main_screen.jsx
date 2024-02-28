5df8019b76d5cd6bb3aee4f82e1f8d30fcaf8517
Neson55 <1385463@mail.ru>
2024-02-28 16:04:55 +0400

    Add Button

--- src/components/Main_screen.jsx
+++ src/components/Main_screen.jsx
@@ -1,4 +1,5 @@
 import Typewriter from 'typewriter-effect';
+import { motion } from 'framer-motion';
 
 function Main_screen() {
 
@@ -8,18 +9,48 @@ function Main_screen() {
                 <Typewriter
                     onInit={(typewriter) => {
                         typewriter.typeString('Neson')
-                            .pauseFor(2500)
-                            .typeString('<br/>')
-                            .pauseFor(2500)
-                        .typeString("I'm professional Web Developer")
-                            .pauseFor(2500)
+                            .pauseFor(1000)
+                            .typeString('<br/>')                            
+                            .typeString("I'm Professional Web Developer")                            
                             .deleteChars(26)
                             .pauseFor(1000)
-                            .typeString("Junior Web Developer") 
+                            .typeString("Junior Web Developer")
                             .start();
                     }}
+                // options={{
+                //     autoStart: true,
+                //     loop: true,
+                // }}
                 />
-            
+                <motion.div className="flex justify-between mx-auto mt-10"
+                    initial="hidden"
+                    animate="visible"
+                    variants={{
+                        hidden: {
+                            scale: 0.8,
+                            opacity: 0,
+                        },
+                        visible: {
+                            scale: 1,
+                            opacity: 1,
+                            transition: {
+                                delay: 15,
+                            },
+                        },
+                    }}
+                >
+                    <a
+                        className='px-12 py-3 text-sm font-medium rounded shadow bg-gray-50 active:bg-gray-900 active:text-slate-50 focus:outline-none focus:ring'
+                    >
+                        Contact Me
+                    </a>
+                    <a
+                        className='px-12 py-3 text-sm font-medium rounded shadow bg-gray-50 active:bg-gray-900 active:text-slate-50 focus:outline-none focus:ring'
+                    >
+                        My resume
+                    </a>
+                </motion.div>
+
             </div>
         </div>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar () {
 
         return(
             <div className='h-1/10 flex justify-between p-4 '>
-                <a  className = 'font-bold text-2xl ml-10' href='#'>Neson</a>
-                <div className='flex text-xl gap-4 mr-10'> 
+                <a  className = 'hover:underline font-medium text-2xl ml-10' href='#'>Neson</a>
+                <div className='flex text-xl gap-4 mr-20'> 
                     <a className='hover:underline' href='#'>Home</a>
                     <a className='hover:underline'href='#'>About</a>
                     <a className='hover:underline'href='#'>Projects</a>
